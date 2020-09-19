c3ef6424b1ae32c4312d80ea65b19ba1afeb3d15
asadalibest1 <asadalibest1@gmail.com>
2020-09-19 16:20:05 +0500

    initial commit

--- src/Dateb.ai/components/services.js
+++ src/Dateb.ai/components/services.js
@@ -25,7 +25,14 @@ export default function Services() {
                             </div>
 
                         </div>
-                        <div className="info"></div>
+                        <div className="info">
+                        <p>
+                        Dateb is providing static website service
+                        with a very low prize. We can help you to                         
+                        create an online presence with your Products.
+                         contact us for getting your web developed. 
+                        </p>
+                        </div>
 
                     </div>
                 </Grid>
@@ -47,7 +54,15 @@ export default function Services() {
                                 <h1>Fully Responsive WebSite </h1>
                                 </div>
                         </div>
-                        <div className="info"></div>
+                        <div className="info">
+                        <p>
+                            Hire us for your website development
+                            project. We develop from E-commerce to
+                            Professional website with neat work and great design & let your
+                            services across the 
+                            globe in many different sectors.
+                        </p>
+                        </div>
                     </div>
                 </Grid>
 
@@ -68,13 +83,20 @@ export default function Services() {
                             <h1>Customize Website, JavaScript, React</h1>
                             </div>
                         </div>
-                        <div className="info"></div>
+                        <div className="info">
+                        <p> 
+                            If you already have a Website 
+                            but Perhaps you want some additional features, 
+                            Dateb is providing Design Customization Service  
+                            Contact us so we can Customize your Website as you like.
+                        </p>
+                        </div>
                     </div>
                 </Grid>
         </Grid>
         <Grid container>
                 <Grid className="serviceGrid" item xs={12} sm={4}
-                    data-aos="fade-up"
+                data-aos="fade-up"
                 data-aos-offset="200"
                 // data-aos-delay="50"
                 data-aos-duration="400"
@@ -86,21 +108,22 @@ export default function Services() {
                     <div className="service">
                         <div className="background background4">
                             <div>
-
-                            <h1>Business Solutions</h1>
+                                <h1>Business Solutions</h1>
                             </div>
                         </div>
                         <div className="info">
-                            <p>We ensure our customers 100%</p>
-                            <p>satisfactory results,  that strengthens</p>
-                            <p>their business through our affordable</p>
-                            <p>and unique solution-packages</p> 
+                            <div>
+                            We ensure our customers 100%
+                            satisfactory results,  that strengthens
+                            their business through our affordable
+                            and unique solution-packages
+                            </div> 
                         </div>
                         </div>
                 </Grid>
 
                 <Grid className="serviceGrid" item xs={12} sm={4}
-                    data-aos="fade-up"
+                data-aos="fade-up"
                 data-aos-offset="200"
                 data-aos-delay="200"
                 data-aos-duration="400"
@@ -117,16 +140,16 @@ export default function Services() {
 
                         </div>
                         <div className="info">
-                            <p>Data Mining, Web Scraping, Risk</p>
-                            <p>Management, Fraud Detection,</p>
-                            <p>Forecast, and other complete</p>
-                            <p>business solutions</p>
+                            <div>Data Mining, Web Scraping, Risk
+                            Management, Fraud Detection,
+                            Forecast, and other complete
+                            business solutions</div>
                         </div>
                         </div>
                 </Grid>
 
                 <Grid className="serviceGrid" item xs={12} sm={4}
-                    data-aos="fade-up"
+                data-aos="fade-up"
                 data-aos-offset="200"
                 data-aos-delay="400"
                 data-aos-duration="400"
